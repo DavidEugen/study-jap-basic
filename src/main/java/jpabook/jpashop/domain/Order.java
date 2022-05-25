72e57abe254649b8bc28cfcaaea96e6cb77e343b
DavidEugen <platonicojju@gmail.com>
2022-05-25 21:48:27 +0900

    Feat: 실전 - 연관관계 맴핑 - 1. 단방향 연관관계 설정
    1. 단방향 연관관계 설정
    
    Member 와 Order 관계에서는 Order가 MEMBER_ID(FK)를 가지므로 Order 가 주인
    
    Order와 OrderItem 관계에서는 OrderItem이 ORDER_ID(FK)를 가지므로 OrderItem이 주인
    
    OrderItem과 Item의 관계에서는 OrderItem이 ITEM_ID(FK)를 가지므로 OrderItem이 주인

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -7,6 +7,8 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,11 +19,47 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
