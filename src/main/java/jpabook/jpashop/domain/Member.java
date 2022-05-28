2d9e803021edb0aa77f7005a7a026819074b99dd
DavidEugen <platonicojju@gmail.com>
2022-05-28 21:51:30 +0900

    Feat: 값 타입 설정
    Entity로 쓰지는 않지만 객체로 따로 관리 할때
    
    정의는 @Embeddable
    사용하는 필드에서는 @Embedded
    
    기본 생성자 필수로 만들어 줘야 함

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -22,9 +23,10 @@ public class Member extends BaseEntity {
     private List<Order> Orders = new ArrayList<>();
 
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address address;
+
 
     public Long getId() {
         return id;
@@ -42,27 +44,19 @@ public class Member extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public List<Order> getOrders() {
+        return Orders;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setOrders(List<Order> orders) {
+        Orders = orders;
     }
 
-    public String getStreet() {
-        return street;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
